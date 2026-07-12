34770908fa748c22cfb48d4b9d1dfd05515c5665
Tristan Ivan Bautista <tristanivanbautista81@gmail.com>
2026-07-12 13:38:16 +0800

    sum, avg, group by lesson exercises

--- S4_Aggregations.sql
+++ S4_Aggregations.sql
@@ -21,3 +21,102 @@ WHERE o.status = 'Completed'
 GROUP BY c.customer_name
 HAVING COUNT(*) >= 2
 ORDER BY total_completed_orders DESC;
+
+
+
+
+-- sum 
+select sum(amount) as tristan_sales 
+from sales 
+where customers = "itan"; 
+
+
+
+select
+	c.customer_name, 
+    sum(o.total) as total_spent 
+from customer c 
+inner join orders o 
+on c.customer_id = o.customer_id 
+group by c.customer_name; 
+
+
+
+SELECT
+    seller,
+    SUM(amount) AS total_sales
+FROM sales
+GROUP BY seller;
+
+
+
+-- AVG 
+SELECT  avg(price) as average_price 
+from products; 
+
+SELECT
+	Employee_name, 
+    AVG(amount) as average_sales 
+FROM sales 
+GROUP BY Employees;
+
+Select 
+	e.employee_name, 
+    avg(s.amount) as average_sales 
+FROM employees e 
+INNER JOIN sales s 
+on e.employee_id = s.employee_id 
+GROUP BY e.employee_name; 
+
+
+
+
+-- group by 
+SELECT
+    employee,
+    SUM(amount)
+FROM sales
+GROUP BY employee;
+
+SELECT
+    employee,
+    SUM(amount) AS total_sales
+FROM sales
+GROUP BY employee;
+
+
+SELECT
+    employee,
+    SUM(amount) AS total_sales
+FROM sales
+WHERE sale_month = 'July'
+GROUP BY employee;
+
+
+
+SELECT
+    c.customer_name,
+    SUM(o.total) AS total_orders
+FROM customers c
+INNER JOIN orders o
+ON c.id = o.customer_id
+WHERE c.city = 'Manila'
+GROUP BY c.customer_name;
+
+
+SELECT
+    employee,
+    SUM(amount) AS total_sales
+FROM sales
+GROUP BY employee
+HAVING SUM(amount) > 3000;
+
+
+
+
+
+
+
+
+
+
